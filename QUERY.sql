89951cf219444bbf382cb3776f811f57a19d9239
Prometheus2003 <samiulmahmud14@gmail.com>
2026-06-15 23:59:18 +0600

    query 7

--- QUERY.sql
+++ QUERY.sql
@@ -158,4 +158,16 @@ WHERE
       bookings
   );
 
+------------------------------------------------------------------
+Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+SELECT
+  match_id,
+  fixture,
+  base_ticket_price
+FROM
+  matches
+ORDER BY
+  base_ticket_price DESC offset 1
+LIMIT
+  2
 
